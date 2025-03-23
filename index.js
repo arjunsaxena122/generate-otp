20630304a2900832ed6ab76fd6a10f212c3e1aa1
arjunsaxena122 <arjunsaxena122@gmail.com>
2025-03-23 22:50:41 +0530

    ADD: New Functionality

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const generateOtp = (digit) =>{
-    let colletDigit = ""
-    for(let i = 0 ; i < digit ; i++){
-        colletDigit += Math.floor(Math.random() * 10)
-    }
-    return colletDigit
+const lowerAplha = "abcdefghijklmnopqrstuvwxyz";
+const upperAplha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const number = "0123456789";
+
+function randNumber(max = 6) {
+  if (isNaN(max)) throw new Error("Please enter the number");
+  return Math.round(Math.random() * Number(max));
 }
+
+function generateOtpFromSet(charStr, length) {
+  let otp = "";
+  while (0 < length) {
+    otp += charStr[randNumber(charStr.length - 1)];
+    length--;
+  }
+  return otp;
+}
+
+const generateOtp = {
+  numeric: (length) => generateOtpFromSet(number, length),
+
+  lowAplha: (length) => generateOtpFromSet(lowerAplha, length),
+
+  capsAlpha: (length) => generateOtpFromSet(upperAplha, length),
+
+  lowCapsAplha: (length) => generateOtpFromSet(lowerAplha + upperAplha, length),
+
+  lowAplhaNumeric: (length) => generateOtpFromSet(number + lowerAplha, length),
+
+  capsAplhaNumeric: (length) => generateOtpFromSet(number + upperAplha, length),
+};
 
 export default generateOtp;
